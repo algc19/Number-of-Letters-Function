3fdc0ca1ea7b03f00abbc7ed0c48ba561a2e2894
algc19 <alvarogc_99@hotmail.com>
2016-09-13 16:17:39 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,7 @@
-// 2 ciclos; 4 <> 6 // 5
-// Todas las cadenas hasta 2444 son =< 4
-// Primera cadena == 5; (2444, 34, 14, 7, 5)
-
-// De 1 a 100.000 todas las cadenas son =< 5
-// A partir de 3000, todos los intervalos de 500 tienen cadenas de 5, y ninguno cadenas de 2 desde 2000
-
-// Cuando llegamos a 30.000 (cadena de 3), las cadenas de 3 escasean
-
-// Hasta 1.000, los numeros convergen el 56.50% de veces en 4, el 33.30% en 5 y el 10.20% en 6
-// Hasta 10.000, los numeros convergen el 69.15% de veces en 4, el 26.70% en 5 y el 4.15% en 6
-// Hasta 15.000, los numeros convergen el 69.05% de veces en 4, el 27.25% en 5 y el 3.70% en 6
-// Hasta 50.000, los numeros convergen el 64.12% de veces en 4, el 34.06% en 5 y el 1.82% en 6
-// Hasta 100.000, los numeros convergen el 63.34% de veces en 4, el 35.09% en 5 y el 1.56% en 6
+// If you are going to run this, do one of these things:
+// 	1. Uncomment lines 18 to 25 to see the graph.
+//	2. Uncomment lines 31, 32 and 151 to check which are the largests and smallest chains between mi and mi + interval. Paste this in the console to check which numbers have chains of lenght X: ´cadenas({len:    X    }).get()´. Type ´Next()´ in the console to check the next interval.
+//	3. Uncomment lines 31, 32 and 152 to check how many of the numbers in the interval reach 4, how many reach 5 and how many reach 6. Type ´Next()´ in the console to check the next interval.
 
 var I;
 var mi = 1;
@@ -23,94 +13,23 @@ var fourCount = 0;
 var sixCount = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  background(150);
-
+  // This plots the graphic:
+  //
+  // createCanvas(400, 400);
+  // background(150);
+  //
   // Plotting(56.5, 69.15, 69.05, 64.12, 63.34, 4)
   // Plotting(33.3, 26.7, 27.25, 34.06, 35.09, 5)
   // Plotting(10.2, 4.15, 3.7, 1.82, 1.56, 6)
   //
   // Plotting(44.9, 47.93, 48.15, 49.09, 49.09, 1)
 
+
+  // This calculates the largest and the smallest chain when the numbers between mi and mi + interval are plotted into the formula OR it calculates the amount of numbers that reach each loop.
+  // Type "Next()" in the console to check the next interval.
+  //
   // noCanvas();
   // Run(mi);
-}
-
-function Plotting(y1, y2, extraY, y3, y4, st) {
-  y1 = map(y1, 0, 100, height, 0);
-  y2 = map(y2, 0, 100, height, 0);
-  extraY = map(extraY, 0, 100, height, 0);
-  y3 = map(y3, 0, 100, height, 0);
-  y4 = map(y4, 0, 100, height, 0);
-  x1 = map(1000, 0, 100000, 10, width);
-  x2 = map(10000, 0, 100000, 10, width);
-  extraX = map(15000, 0, 100000, 10, width);
-  x3 = map(50000, 0, 100000, 10, width);
-  x4 = map(100000, 0, 100000, 10, width);
-
-  var r = 5;
-
-  if (st == 4) {
-    stroke(255, 0, 0)
-    fill(255, 0, 0)
-  } else if (st == 5) {
-    stroke(0, 255, 0)
-    fill(0, 255, 0)
-  } else if (st == 6) {
-    stroke(0, 0, 255)
-    fill(0, 0, 255)
-  } else {
-    stroke(0)
-    fill(0, 0)
-    r = 2
-  }
-  ellipse(x1, y1, r);
-  ellipse(x2, y2, r);
-  ellipse(extraX, extraY, r);
-  ellipse(x3, y3, r);
-  ellipse(x4, y4, r);
-  noFill();
-  curve(10,height,10,height,x1,y1,x2,y2);
-  curve(10,height,x1,y1,x2,y2,x3,y3);
-  curve(x1,y1,x2,y2,x3,y3,x4,y4);
-  curve(x2,y2,x3,y3,x4,y4,x4,y4);
-}
-
-function Next() {
-  mi += interval;
-  Run(mi);
-}
-
-function CalcLargestChain() {
-  var mx = cadenas().max("len");
-  var mn = cadenas().min("len");
-  console.log("Max " + mx + ",  Min " + mn)
-  // cadenas({len:m}).get()
-}
-
-function Run(mi) {
-  cadenas = TAFFY([]);
-  for (var i = mi; i < interval + mi; i++) {
-    I = i;
-    cadenas.insert({num:i})
-     var recursivo = Recursivo(i, "");
-  }
-  // CalcLargestChain();
-  console.log("Cuatro: " + fourCount + "           Cinco: " + fiveCount + "           Seis: " + sixCount);
-}
-
-function Recursivo(i, sec) {
-  if (i == 5 || i == 4 || i == 6) {
-    if (i == 5) {fiveCount ++;} else if (i == 4) {fourCount ++;} else {sixCount ++;}
-    sec += i.toString();
-    var value = sec.split(", ").length;
-    cadenas({num:I}).update({len: value});
-    return;
-  } else {
-    sec += i.toString();
-    sec += ", ";
-    Recursivo(Nombre(i).length, sec);
-  }
 }
 
 function Nombre(i) {
@@ -204,3 +123,82 @@ var baseDatos = TAFFY([
   {num: 900, nom:"novecientos"},
   {num: 1000, nom:"mil"}
 ])
+
+function Recursivo(i, sec) {
+  if (i == 5 || i == 4 || i == 6) {
+    if (i == 5) {fiveCount ++;} else if (i == 4) {fourCount ++;} else {sixCount ++;}
+    sec += i.toString();
+    var value = sec.split(", ").length;
+    cadenas({num:I}).update({len: value});
+    return;
+  } else {
+    sec += i.toString();
+    sec += ", ";
+    Recursivo(Nombre(i).length, sec);
+  }
+}
+
+function Run(mi) {
+  cadenas = TAFFY([]);
+  for (var i = mi; i < interval + mi; i++) {
+    I = i;
+    cadenas.insert({num:i})
+     var recursivo = Recursivo(i, "");
+  }
+  
+  // Choose wheter to calculate the largest chain or the proportion of numbers that reach each loop:
+  //
+  // CalcLargestChain();
+  // console.log("Cuatro: " + fourCount + "           Cinco: " + fiveCount + "           Seis: " + sixCount);
+}
+
+function Next() {
+  mi += interval;
+  Run(mi);
+}
+
+function CalcLargestChain() {
+  var mx = cadenas().max("len");
+  var mn = cadenas().min("len");
+  console.log("Max " + mx + ",  Min " + mn)
+}
+
+function Plotting(y1, y2, extraY, y3, y4, st) {
+  y1 = map(y1, 0, 100, height, 0);
+  y2 = map(y2, 0, 100, height, 0);
+  extraY = map(extraY, 0, 100, height, 0);
+  y3 = map(y3, 0, 100, height, 0);
+  y4 = map(y4, 0, 100, height, 0);
+  x1 = map(1000, 0, 100000, 10, width);
+  x2 = map(10000, 0, 100000, 10, width);
+  extraX = map(15000, 0, 100000, 10, width);
+  x3 = map(50000, 0, 100000, 10, width);
+  x4 = map(100000, 0, 100000, 10, width);
+
+  var r = 5;
+
+  if (st == 4) {
+    stroke(255, 0, 0)
+    fill(255, 0, 0)
+  } else if (st == 5) {
+    stroke(0, 255, 0)
+    fill(0, 255, 0)
+  } else if (st == 6) {
+    stroke(0, 0, 255)
+    fill(0, 0, 255)
+  } else {
+    stroke(0)
+    fill(0, 0)
+    r = 2
+  }
+  ellipse(x1, y1, r);
+  ellipse(x2, y2, r);
+  ellipse(extraX, extraY, r);
+  ellipse(x3, y3, r);
+  ellipse(x4, y4, r);
+  noFill();
+  curve(10,height,10,height,x1,y1,x2,y2);
+  curve(10,height,x1,y1,x2,y2,x3,y3);
+  curve(x1,y1,x2,y2,x3,y3,x4,y4);
+  curve(x2,y2,x3,y3,x4,y4,x4,y4);
+}
